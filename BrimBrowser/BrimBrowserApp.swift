77035fdfcb1f78a89ae310b7f6487885da7439b1
Devansh Rai <idevanshrai@gmail.com>
2025-09-10 15:32:54 +0530

    Initial Commit

---
+++ BrimBrowser/BrimBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrimBrowserApp.swift
+//  BrimBrowser
+//
+//  Created by Devansh Rai on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct BrimBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrimBrowserTests/BrimBrowserTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrimBrowserTests.swift
+//  BrimBrowserTests
+//
+//  Created by Devansh Rai on 10/9/25.
+//
+
+import Testing
+@testable import BrimBrowser
+
+struct BrimBrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
